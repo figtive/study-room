1f403f3f58a08f3e9cec9d563fd6da2900897b3f
Aufa Wiandra Moenzil <aufawiandra@gmail.com>
2018-10-17 19:54:29 +0700

    add news

--- news/views.py
+++ news/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .models import News
+
+
+def news(request):
+    all_news = News.objects.all()
+    response = {
+        'all_news': all_news,
+    }
+    return render(request, 'news.html', response)
