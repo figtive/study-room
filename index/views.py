fc295a06735fced8e480cceb19020ec795f84e71
Danny August Ramaputra <august_danny@yahoo.co.id>
2018-10-17 21:52:02 +0700

    added implementation for news and events on index page

--- index/views.py
+++ index/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from event.models import Event
+from news.models import News
 
 def index(request):
     response = {
-        'all_event': Event.objects.all().order_by("date")
+        'all_event': Event.objects.all().order_by("date"),
+        'all_news': News.objects.all().order_by("-date")
     }
     return render(request, 'index.html', response)
--- news/models.py
+++ news/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 
 class News(models.Model):
-    news_title = models.CharField(max_length=50)
-    news_author = models.CharField(max_length=50)
-    news_date = models.DateField(default=timezone.now)
-    news_content = models.TextField()
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    date = models.DateField(default=timezone.now)
+    content = models.TextField()
