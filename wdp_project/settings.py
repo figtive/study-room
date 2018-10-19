e6f5481f0e96e2e78dbf38f27f67b5c3ff33de8e
Danny August Ramaputra <august_danny@yahoo.co.id>
2018-10-19 15:12:49 +0700

    fixed pipeline coverage test, removed unneeded tag extension

--- wdp_project/settings.py
+++ wdp_project/settings.py
@@ -69,10 +69,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-
-            'libraries': {
-                'event_extras': 'event.template_tags.event_extras'
-            },
         },
     },
 ]
