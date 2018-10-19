d018391fb526f9fcc7ce343a3f98a2119589faf6
Michael Sudirman <astradon.michael@gmail.com>
2018-10-19 08:51:37 +0700

    create extension templates

--- event/models.py
+++ event/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class Event(models.Model):
     name = models.CharField(max_length=30, blank=False, null=True)
-    date = models.DateField()
+    date = models.DateField(default=timezone.now())
     location = models.CharField(max_length=50, blank=False, null=True)
     # description is used for aesthetics. Discard if this destroys flexibility 
     description = models.CharField(max_length=200, blank=False, null=True)
--- event/tests.py
+++ event/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 
 # Create your tests here.
+from django.db import models
+from django.utils import timezone
+
+from django.urls import resolve
+from .views import event
+from .models import Event
+
+class Event_Test(TestCase):
+    def test_event_is_exist(self):
+        response = Client().get('/event/')
+        self.assertEqual(response.status_code,200)
+    
+    def test_url_not_exist(self):
+        response = Client().get('doesnotexist')
+        self.assertEqual(response.status_code,404)
+
+    def test_check_model_name(self):
+        event = Event(name="Name", date=timezone.now(),  location="loc", description="desc")
+        event_name = str(event)
+        self.assertEqual(event_name,"Name")
