6ce31d5604f5ff9467815d2a08b139bf758e9b49
Aufa Wiandra Moenzil <aufawiandra@gmail.com>
2018-10-20 13:06:50 +0700

    add news feature

--- news/models.py
+++ news/models.py
@@ -4,7 +4,8 @@ from django.utils import timezone
 # Create your models here.
 
 class News(models.Model):
-    title = models.CharField(max_length=50)
-    author = models.CharField(max_length=50)
-    date = models.DateField(default=timezone.now)
-    content = models.TextField()
+    title = models.CharField(max_length=50, blank=False)
+    author = models.CharField(max_length=50, blank=False)
+    date = models.DateField(default=timezone.now, blank=False)
+    content = models.TextField(blank=False)
+    headline = models.CharField(max_length=50, blank=False)
