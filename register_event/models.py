8f27b377436cd41d4943f6905bee9bb16e68d270
Arief Kusuma <ariefkusuma@Ariefs-MacBook-Air.local>
2018-10-23 18:05:13 +0700

    edited the register events section

--- register_event/models.py
+++ register_event/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models.signals import post_save
 
 # Create your models here.
+FACULTIES = [
+    ('FK', 'FK'),
+    ('FKG', 'FKG'),
+    ('FKM', 'FKM'),
+    ('FIK', 'FIK'),
+    ('FF', 'FF'),
+    ('FMIPA', 'FMIPA'),
+    ('FT', 'FT'),
+    ('FASILKOM', 'FASILKOM'),
+    ('FH', 'FH'),
+    ('FEB', 'FEB'),
+    ('FIB', 'FIB'),
+    ('FPsi', 'FPsi'),
+    ('FISIP', 'FISIP'),
+    ('FIA', 'FIA'),
+    ('VOKASI', 'VOKASI'),
+]
+
+class UnionEvent(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(max_length=50, blank=False, null=False)
+    student_id = models.PositiveIntegerField(blank=False, null=False)
+    faculty = models.CharField(max_length=10, choices=FACULTIES, blank=False, null=False)
--- register_event/views.py
+++ register_event/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from .forms import RegisterEvent
+from .models import UnionEvent
+
+
 
 # Create your views here.
+
+def register_event(request):
+    posted = UnionEvent.objects.all()
+    if (request.method == 'POST'):
+        form = RegisterEvent(request.POST)
+            if (form.is_valid()):
+
+                form.save()
+
+                return HttpResponseRedirect('/event/register/')
+
+    else :
+
+        form = RegisterEvent()
+        context = {
+        'Posts' : posted,
+        'RegisterEvent' : form,
+        }      
+
+    return HttpResponseRedirect('/event/register/')        
+        
