874a5a12940d37748e65d7610cc212ff18e0d10b
Danny August Ramaputra <august_danny@yahoo.co.id>
2018-12-07 15:14:39 +0700

    Added missing import

--- register_user/views.py
+++ register_user/views.py
@@ -9,6 +9,7 @@ from django.core.mail import EmailMultiAlternatives
 
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
+from django.utils.html import strip_tags
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from .tokens import account_activation_token
