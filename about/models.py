038e9d90ef138ea99b3e4d6a565e5dac68a24097
Aufa Wiandra Moenzil <aufawiandra@gmail.com>
2018-12-07 16:59:40 +0700

    add tests for testimonies

--- about/models.py
+++ about/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
-
+from register_user.models import UnionMember
 
 # Create your models here.
 
 class About(models.Model):
-    user = models.OneToOneField(User, blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(UnionMember, blank=True, on_delete=models.CASCADE)
     content = models.TextField(blank=False)
     date = models.DateField(default=timezone.now, blank=False)
